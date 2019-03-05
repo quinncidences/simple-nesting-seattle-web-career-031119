dc63f3bb8da72aff31dee48c0d90aa6b2c25e5e5
Quinn Cox <qrc.inpa@gmail.com>
2019-03-05 03:48:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,11 +63,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash =
  		{
@@ -84,7 +79,15 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+		 # add the following information to the top level of programmer_hash
+		 # :yukihiro_matsumoto => {
+		 #   :known_for => "Ruby",
+		 #    :languages => ["LISP", "C"]
+		 # }
+		 programmer_hash => [:yukihiro_matsumoto] = {
+			 											[:known_for] => "Ruby",
+														[:languages] => ["LISP", "C"]
+		 }
 
 end
 
